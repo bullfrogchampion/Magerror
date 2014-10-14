8360173e1db22da563128d301ae061111a9add7f
Jeremy Champion <bullfrogchampion@gmail.com>
2014-10-14 11:00:41 +0000

    Fix broken require line in observer method
    Oops. See point about this line in the roadmap in the readme.

--- src/app/code/community/Champion/Magerror/Model/Observer.php
+++ src/app/code/community/Champion/Magerror/Model/Observer.php
@@ -4,7 +4,7 @@ class Champion_Magerror_Model_Observer
 {
     function registerPhpError($observer)
     {
-        require_once(dirname(__FILE__) . '/../../../../../../../../../joseph-lenton/php-error/src/php_error.php');
+        require_once(dirname(__FILE__) . '/../../../../../../../../../bullfrogchampion/php-error/src/php_error.php');
         \php_error\reportErrors();
     }
 }
